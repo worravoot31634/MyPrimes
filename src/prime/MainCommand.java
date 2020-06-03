5b1efd06a96c10528cc820624ccc97091bd5562d
worravoot31634 <55147927+worravoot31634@users.noreply.github.com>
2020-06-03 19:30:57 +0700

    Update CPU Time

--- src/prime/MainCommand.java
+++ src/prime/MainCommand.java
@@ -1,60 +1,78 @@
 package prime;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 
 public class MainCommand {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		/** check arguments not less than zero*/
+		/** check arguments not less than zero */
 		if (args.length > 0) {
 			/** make sure that arguments not empty */
 			if (args[0].isEmpty()) {
 				System.err.println(args[0] + " is empty");
-				
+
 			} else { /** if arguments not empty */
-				
-				/** Declare variables*/
+
+				/** Declare variables */
 				int FindFrom = Integer.parseInt(args[0]);
 				int FindTo = Integer.parseInt(args[1]);
 				int SIZE = FindTo - FindFrom;
 				int numThread = 0;
 				int i = 0;
-				
-				
-				
+
 				/** Declare and Initial array size of Thread */
 				Thread[] thrd = new Thread[SIZE + 1];
-
-				
 
 				/** make sure that start number will not equal zero */
 				if (FindFrom == 0) {
 					FindFrom = FindFrom + 1;
 				}
-				/** Start count wall clock time in nano seconds*/
+
+				/** Start count wall clock time in nano seconds */
 				long startTimeNano = System.nanoTime();
-				
+
 				/** Create each Thread for each number */
 				for (i = 0; i <= SIZE; i++) {
 					/** Initial Threads */
 					thrd[i] = new Thread(new PrimeThread(FindFrom++, SIZE));
-					/** run thread*/ 
+					/** run thread */
 					thrd[i].start();
-					/** join thread*/
+					
+					/** join thread */
 					thrd[i].join();
 				}
 				
+		       
+				
+				
 
-				/** Stop count wall clock time in nano seconds*/
+				/** Stop count wall clock time in nano seconds */
 				long taskTimeNano = System.nanoTime() - startTimeNano;
 				System.out.print("Wall Clock Time: " + taskTimeNano + " nano seconds");
-				
+
 			}
 
 		}
 
 	}
 
-	
+	/** Get CPU time in nanoseconds. */
+	public static long getCpuTime(long thrdID) {
+		ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+		return bean.isCurrentThreadCpuTimeSupported() ? bean.getThreadCpuTime(thrdID) : 0L;
+	}
 
+}
+
+
+class MyThread extends Thread {
+    public void run() {
+        int sum = 0;
+        for (long i = 0; i < 1000000; ++i) {
+            sum += i;
+        }
+        sum = sum + 1;
+    }
 }
--- src/prime/PrimeThread.java
+++ src/prime/PrimeThread.java
@@ -4,15 +4,14 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 
 public class PrimeThread implements Runnable {
-	
-	/** Declare variables*/
+
+	/** Declare variables */
 	private long cpu = -1;
 	private int number;
 	private int notPrimeNumber;
 	private int size;
 	private int countCol;
 	private long ThreadID;
-	
 
 	public PrimeThread(int number, int size) {
 		this.number = number;
@@ -20,21 +19,14 @@ public class PrimeThread implements Runnable {
 	}
 
 	@Override
-	public synchronized void run() {
-
-		/** Start for get CPU time each Thread */
-		ThreadMXBean thread = ManagementFactory.getThreadMXBean();
-        long cpu = thread.getCurrentThreadCpuTime();
-        long startTime = System.nanoTime();
-        this.cpu = thread.getCurrentThreadCpuTime() - cpu;
-       
-         
-        /** call method isPrime for check prime number*/
+	public void run() {
+		long cpuTimeBegin = getCpuTime();
+		/** call method isPrime for check prime number */
 		if (isPrime(number)) {
-			/** if method return true, this number is prime number*/
+			/** if method return true, this number is prime number */
 			System.out.println("number: " + number + " is Prime number");
 		} else {
-			/** if method return false, this number isn't prime number*/
+			/** if method return false, this number isn't prime number */
 			System.out.println("number: " + number + " isn't Prime number ");
 			System.out.println("Devided number is: ");
 			/** Loop for check divisible numbers */
@@ -42,52 +34,59 @@ public class PrimeThread implements Runnable {
 				/** If divisible then print that number */
 				if (notPrimeNumber % i == 0) {
 					System.out.print(i + "\t");
-					/** count column of number*/
+					/** count column of number */
 					countCol++;
-					/** new line when number stay full in column*/
+					/** new line when number stay full in column */
 					if (countCol == 20) {
-						/** reset count column*/
+						/** reset count column */
 						countCol = 0;
 						System.out.print("\n");
 					}
 				}
-
 			}
 		}
-		/** Stop for get CPU time each Thread */
-		 long stopTime = System.nanoTime();
-		 
-		 /** Show CPU Time each threads*/
-		System.out.println("\nCPU Time: " + cpu + " nano seconds");
+		
+		
+		
+		
+		/** Show CPU Time each threads */
+		long cputimeEnd = getCpuTime() - cpuTimeBegin;
+		System.out.println("\nCPU Time: " + cputimeEnd  +  " nano seconds");
 		System.out.print("\n\n");
-
+		
+           
 	}
 
 	/** Method check Prime Number **/
 	public boolean isPrime(int num) {
-		/** Declare variables*/
+		/** Declare variables */
 		int i;
 		
-		
-			/** make sure that number must not be an even number*/
-			if (num % 2 == 0) {
-				/** collect even number value for check all divided that even number*/
+
+		/** make sure that number must not be an even number */
+		if (num % 2 == 0) {
+			/** collect even number value for check all divided that even number */
+			notPrimeNumber = num;
+			return false;
+		}
+		/** Loop check Prime number step by two */
+		for (i = 3; i * i <= num; i += 2) {
+			/** If number mod I and equal zero this number isn't prime number */
+			if (num % i == 0) {
+				/** collect number value for check all divided that number */
 				notPrimeNumber = num;
 				return false;
 			}
-			/**Loop check Prime number step by two*/
-			for (i = 3; i * i <= num; i += 2) {
-				/** If number mod I and equal zero this number isn't prime number */
-				if (num % i == 0) {
-					/** collect number value for check all divided that number*/
-					notPrimeNumber = num;
-					return false;
-				}
-			}
+		}
+		
 		
 		return true;
 	}
 
-	
+	/** Get CPU time in nanoseconds. */
+	public long getCpuTime() {
+		ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+		return bean.isCurrentThreadCpuTimeSupported() ? bean.getCurrentThreadCpuTime() : 0L;
+	}
 
 }
